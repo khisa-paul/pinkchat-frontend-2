187e82b8b731f23a27d1221dc06d1d20ec10ea59
khisa-paul <khisapaul@gmail.com>
2025-10-01 13:30:57 +0300

    Update App.js
    update

--- src/App.js
+++ src/App.js
@@ -9,12 +9,14 @@ import ForgotPassword from "./components/ForgotPassword";
 function App() {
   return (
     <Router>
-      <div className="min-h-screen flex items-center justify-center bg-pink-100 px-4">
-        <Routes>
-          <Route path="/" element={<Login />} />
-          <Route path="/register" element={<Register />} />
-          <Route path="/forgot-password" element={<ForgotPassword />} />
-        </Routes>
+      <div className="min-h-screen flex items-center justify-center bg-pink-100">
+        <div className="w-full max-w-md bg-white p-8 rounded-2xl shadow-xl">
+          <Routes>
+            <Route path="/" element={<Login />} />
+            <Route path="/register" element={<Register />} />
+            <Route path="/forgot-password" element={<ForgotPassword />} />
+          </Routes>
+        </div>
       </div>
     </Router>
   );
